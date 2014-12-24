fb73f7124a9bc424ab136d626c35eb3a62245922
lcor <lcormaw@gmail.com>
2014-12-24 15:02:32 -0500

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,53 @@
+function getRandomColor() {
+	var letters = '0123456789ABCDEF'.split(''); //splits each character from a string into an array
+	var color = '#';
+	for (var i = 0; i < 6; i++ ) {
+		color += letters[Math.floor(Math.random() * 16)]; //+= is for appending the color string
+	}
+	return color;
+}
+
+var clickedTime; var createdTime; var reactionTime; var bestTime=1000000;
+
+function makeBox() {
+
+	var time=Math.random()*5000;
+
+	setTimeout(function() {
+
+		var top=Math.random()*600; var left=Math.random()*900
+		
+		document.getElementById("box").style.top=top+"px";
+		
+		document.getElementById("box").style.left=left+"px";
+		
+		document.getElementById("box").style.backgroundColor=getRandomColor();
+		
+		document.getElementById("box").style.display="block";
+		
+		createdTime=Date.now();
+
+	}, time); // time is in milliseconds
+
+}
+
+document.getElementById("box").onclick=function() {
+
+	clickedTime=Date.now();
+	
+	reactionTime=(clickedTime-createdTime)/1000; // time in seconds
+	
+	document.getElementById("time").innerHTML=reactionTime+" seconds";
+	
+	if (reactionTime<bestTime) {
+		bestTime=reactionTime;
+		document.getElementById("best-time").innerHTML=bestTime+" seconds";
+	}
+	
+	this.style.display="none";
+	
+	makeBox();
+
+}
+
+makeBox();
